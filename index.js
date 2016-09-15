e9f502f37633236886fa2ff3e19ce9aafda45ee9
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2016-09-15 09:36:33 +0200

    add temp directory

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const http = require('http')
 const execFile = require('child_process').execFile
 const fs = require('fs')
+const path = require('path')
 const async = require('async')
 const uuid = require('uuid').v1
+const temp = process.env.temp
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
@@ -23,18 +25,18 @@ const server = http.createServer((req, res) => {
 
     const childArgs = [
       '-fo',
-      `${id}.fo`,
+      path.join(temp, `${id}.fo`),
       '-pdf',
-      `${id}.pdf`
+      path.join(temp, `${id}.pdf`)
     ]
 
     const isWin = /^win/.test(process.platform)
     const fopFile = 'fop' + (isWin ? '.bat' : '')
 
     async.waterfall([
-      (cb) => fs.writeFile(`${id}.fo`, opts.fo, cb),
+      (cb) => fs.writeFile(path.join(temp, `${id}.fo`), opts.fo, cb),
       (cb) => execFile(fopFile, childArgs, cb),
-      (stdout, stderr, cb) => fs.stat(`${id}.pdf`, (err) => cb(err, stdout, stderr))
+      (stdout, stderr, cb) => fs.stat(path.join(temp, `${id}.pdf`), (err) => cb(err, stdout, stderr))
     ], (err, stdout, stderr) => {
       if (err) {
         res.statusCode = 500
@@ -42,7 +44,7 @@ const server = http.createServer((req, res) => {
         return res.end('Error when executing fop ' + stdout + stderr + err.stack)
       }
 
-      const stream = fs.createReadStream(`${id}.pdf`)
+      const stream = fs.createReadStream(path.join(temp, `${id}.pdf`))
       stream.pipe(res)
     })
   })
